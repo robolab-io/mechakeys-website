4a3d47229124bae95377c9886df6e11ba4a9a3e3
Evan / Fero <hi@evan.graphics>
2022-07-11 18:57:07 -0700

    chore: download component in ads

--- src/components/Advertising.svelte
+++ src/components/Advertising.svelte
@@ -1,3 +1,7 @@
+<script>
+    import Breathe from '../components/Breathe.svelte'
+    import Downloads from '../components/Downloads.svelte'
+</script>
 <div class="section sectiondark">
     <div class="sectionContent">
         <div class="sectionImage"><img class="image" src="../images/rocket-hero.png" alt=""/></div>
@@ -34,10 +38,29 @@
             <div class="sectionTitle">Download the best keyboard simulator.</div>
             <div class="sectionDescription">MechaKeys is free, forever, for personal use.</div>
         </div>
+        <div class="endingDownloads">
+            <Downloads/>
+            <Breathe/>
+        </div>
     </div>
+</div>
+<div class="section">
+    <div class="copyright">Copyright (c) 2022 Robolab, Fero</div>
 </div>
 
 <style>
+.copyright {
+    color: #8C939E;
+    font-size: 12px;
+    font-family: 'MechaKeys Standard';
+    padding: 1rem;
+}
+.endingDownloads {
+    margin-top: 2rem;
+    align-self: flex-start;
+    display: flex;
+    flex-direction: column;
+}
 .sectionDescription {
     word-wrap: break-word;
     color: #8C939E;
@@ -50,6 +73,7 @@
     animation-direction: normal;
     animation-fill-mode: backwards;
     animation-play-state: running;
+    animation-delay: 350ms;
 }
 .sectionTitle {
     font-size: clamp(20px,3vw,40px);
@@ -61,6 +85,7 @@
     animation-direction: normal;
     animation-fill-mode: backwards;
     animation-play-state: running;
+    animation-delay: 300ms;
 }
 .image {
     width: 100%;
@@ -76,7 +101,6 @@
     animation-direction: normal;
     animation-fill-mode: backwards;
     animation-play-state: running;
-    animation-delay: calc(var(--base-delay) + 150ms); 
 }
 
 .sectionText {
@@ -109,8 +133,8 @@
 
 .endingSectionContent {
     display: flex;
-    flex-direction: row;
-    padding: 4rem;
+    flex-direction: column;
+    padding: 4rem 4rem 0 4rem;
     align-items: center;
 }
 
