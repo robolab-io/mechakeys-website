2b15c3f560a9654b933512863cb7b3414a067101
Evan / Fero <hi@evan.graphics>
2022-07-14 10:54:57 -0700

    fix: build

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,10 @@
-import adapter from "@sveltejs/adapter-static"; 
-// was "@sveltejs/adapter-auto"
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter({
-            pages: "docs",
-            assets: "docs"
-        }),
-		prerender: {
-			default: true,
-		},
-    }
+	kit: {
+		adapter: adapter()
+	},
 };
 
 export default config;
